e0a84e033318c44256249169e6327e0dc7f78ceb
mstars_lab2_08 <mstars_lab2_08@mstars-lab2-08s-iMac.local>
2022-06-16 17:01:07 +0800

    D

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Users = require("../models/Users");
+const Users = require("../models/users");
 const mongoose = require("mongoose");
 const { ObjectId } = require("mongodb");
 const { json } = require("express/lib/response");
